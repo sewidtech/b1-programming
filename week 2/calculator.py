460ba66b329aa822ec1322ac139e3ec03921e352
sewidtech <yehiaagm2007@gmail.com>
2025-10-17 16:52:20 +0200

    Create calculator.py

---
+++ week 2/calculator.py
@@ -0,0 +1,35 @@
+#CalculatorProgram
+print("Welcome to the Business calculator")
+
+
+choice = int(input("Please choose an option:\n1. Revenue\n2. Cost\n3. Profit\n"))
+
+
+#revenue and costs
+Revenue = float(input("Please enter your Revenue:"))
+Cost = float(input("Please enter your Cost:"))
+
+
+Profit = Revenue - Cost
+
+
+
+#OPERATIONS
+sum = (Revenue + Cost)
+subtraction = (Revenue - Cost)
+multiplication = (Revenue * Cost)
+division = (Revenue / Cost)
+
+
+display_results = bool(input("Do you want to see the results? (yes/no): "))
+
+if choice == 1:Revenue
+print("The result is:" ,Revenue)
+if choice == 2:Cost
+print("The result is:" ,Cost)
+if choice == 3:Profit
+print("The result is:" ,Profit)
+
+
+
+#RESULTS

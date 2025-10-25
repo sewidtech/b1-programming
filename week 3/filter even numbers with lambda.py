7f81e8a608c8442d2630ea06a74282df819379c7
sewidtech <yehiaagm2007@gmail.com>
2025-10-26 00:51:43 +0200

    Update filter even numbers with lambda.py

--- week 3/filter even numbers with lambda.py
+++ week 3/filter even numbers with lambda.py
@@ -1,5 +1,5 @@
 #filter even numbers
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-even = list(filter(lambda x : x % 2 , numbers ))
+even = list(filter(lambda x : x % 2 == 0 , numbers ))
 print(even)
